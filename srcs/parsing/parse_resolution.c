fe35d739fe4d3c8488f22d3c93efc4390bae12fd
Nicolas Catriens <nicolas.catriens@gmail.com>
2021-04-29 13:08:37 +0200

    fixing leak in parse_resolution

--- srcs/parsing/parse_resolution.c
+++ srcs/parsing/parse_resolution.c
@@ -6,7 +6,7 @@
 /*   By: ncatrien <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 11:19:21 by ncatrien          #+#    #+#             */
-/*   Updated: 2021/04/28 10:48:33 by ncatrien         ###   ########lyon.fr   */
+/*   Updated: 2021/04/29 12:37:26 by ncatrien         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,23 @@
 void	parse_resolution(char *line, t_scene *scene)
 {
 	char	**split;
-	int		x;
-	int		y;
 
 	if (scene->width != 0)
+	{
 		set_error(scene, E_ALREADY_EXISTS);
+		return ;
+	}
 	split = get_split(line, " \t", 3, scene);
 	if (split && scene->valid)
 	{
-		x = (int)get_positive_val(split[1]);
-		y = (int)get_positive_val(split[2]);
-		if (x <= 0 || y <= 0)
+		scene->width = (int)get_positive_val(split[1]);
+		scene->height = (int)get_positive_val(split[2]);
+		if (scene->width <= 0 || scene->height <= 0)
 			set_error(scene, E_INVAL);
-		if (x > RESOLUTION_MAX || y > RESOLUTION_MAX)
+		if (scene->width > RESOLUTION_MAX || scene->height > RESOLUTION_MAX)
 			set_error(scene, E_IMG_SIZE_MAX);
-		scene->width = x;
-		scene->height = y;
 		if (scene->valid)
-			scene->ratio = x / (double)y;
+			scene->ratio = scene->width / (double)scene->height;
 		free_split(split);
 	}
 	else
